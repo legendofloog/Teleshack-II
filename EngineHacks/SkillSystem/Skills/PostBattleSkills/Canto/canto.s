e8230efa120fdb37fa62ed66550288fbc1de1a07
legendofloog <77558732+legendofloog@users.noreply.github.com>
2021-07-20 20:56:40 -0700

    attempt at move stars
    Movement stars ASM attempted but crashes. Other miscellaneous changes made

--- EngineHacks/SkillSystem/Skills/PostBattleSkills/Canto/canto.s
+++ EngineHacks/SkillSystem/Skills/PostBattleSkills/Canto/canto.s
@@ -41,12 +41,12 @@ beq	End
 @ldrb  r0, [r6,#0x11]  @action taken this turn
 @cmp r0, #0x04 @check if staff or attack was used
 @blo End
-@cmp r0, #0x1E @check if found enemy in the fog
-@beq End
-@ldrb  r0, [r6,#0x0C]  @allegiance byte of the current character taking action
-@ldrb  r1, [r4,#0x0B]  @allegiance byte of the character we are checking
-@cmp r0, r1    @check if same character
-@bne End
+cmp r0, #0x1E @check if found enemy in the fog
+beq End
+ldrb  r0, [r6,#0x0C]  @allegiance byte of the current character taking action
+ldrb  r1, [r4,#0x0B]  @allegiance byte of the character we are checking
+cmp r0, r1    @check if same character
+bne End
 
 @check if already cantoing
 ldr	r0, [r4,#0x0C]	@status bitfield
