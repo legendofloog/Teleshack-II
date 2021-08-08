8da5e2ee938eed906e88d6bd918297f70a0cc93a
legendofloog <77558732+legendofloog@users.noreply.github.com>
2021-08-08 01:52:14 -0700

    pcc broke
    pushing early because of an issue where wrong pcc skills are assigned. chapter still in progress

--- Text/AssembleText.cmd
+++ Text/AssembleText.cmd
@@ -4,6 +4,6 @@
 set "textprocess=%~dp0..\Tools\TextProcess\text-process-classic"
 set "parsefile=%~dp0..\Event Assembler\Tools\ParseFile.exe"
 
-echo: | ("%textprocess%" "text_buildfile.txt" --parser-exe "%parsefile%")
+echo: | ("%textprocess%" "text_buildfile.event" --parser-exe "%parsefile%")
 
 pause
