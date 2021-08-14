813ea51013089b90f83369ec6908bdda08c7f243
legendofloog <77558732+legendofloog@users.noreply.github.com>
2021-08-13 19:32:12 -0700

    pushing for dismount

---
+++ EngineHacks/TeleshackII/ThraciaASM/Dismount/Dismount.c
@@ -0,0 +1,84 @@
+
+#include "gbafe.h"
+#include "Dismount.h"
+
+void DismountRoutine(Proc* procState){
+	Unit* unit = gActiveUnit;
+	UnitChangeClass(unit, GetDismountedClass(unit));
+	ProcGoto(procState, 1);
+}
+
+void MountRoutine(Proc* procState){
+	Unit* unit = gActiveUnit;
+	UnitChangeClass(unit, GetMountedClass(unit));
+	ProcGoto(procState, 1);
+}
+
+int DismountUsability(){
+	Unit* unit = gActiveUnit;
+	if (GetDismountedClass(unit)){
+		if(GetDismountedClass(unit)->pMovCostTable[0][gMapTerrain[unit->yPos][unit->xPos]] != 0xFF){
+			return USABILITY_TRUE;
+		}
+	}
+	return USABILITY_FALSE;
+}
+
+int MountUsability(){
+	Unit* unit = gActiveUnit;
+	if (GetMountedClass(unit)){
+		if(GetMountedClass(unit)->pMovCostTable[0][gMapTerrain[unit->yPos][unit->xPos]] != 0xFF){
+			return USABILITY_TRUE;
+		}
+	}
+	return USABILITY_FALSE;
+}
+
+const ClassData* GetDismountedClass(Unit* unit){
+	int mountedNumber = unit->pClassData->number;
+	int cnt = 0;
+
+	while(true){
+		if (MountedClassTable[cnt].mountedNumber == mountedNumber){
+			return GetClassData(MountedClassTable[cnt].dismountedNumber);
+		}
+		else if (MountedClassTable[cnt].mountedNumber == 0){
+			return 0;
+		}
+		cnt++;
+	}
+}
+
+const ClassData* GetMountedClass(Unit* unit){
+	int dismountedNumber = unit->pClassData->number;
+	int cnt = 0;
+
+	while(true){
+		if (MountedClassTable[cnt].dismountedNumber == dismountedNumber){
+			return GetClassData(MountedClassTable[cnt].mountedNumber);
+		}
+		else if (MountedClassTable[cnt].mountedNumber == 0){
+			return 0;
+		}
+		cnt++;
+	}
+}
+
+void UnitChangeClass(Unit* unit, const ClassData* newClass){
+	const ClassData* oldClass = unit->pClassData;
+
+	unit->maxHP += (newClass->baseHP - oldClass->baseHP);
+	unit->curHP += (newClass->baseHP - oldClass->baseHP);
+	unit->pow += (newClass->basePow - oldClass->basePow);
+	unit->mag += (MagClassTable[newClass->number].baseMag - MagClassTable[oldClass->number].baseMag);
+	unit->skl += (newClass->baseSkl - oldClass->baseSkl);
+	unit->spd += (newClass->baseSpd - oldClass->baseSpd);
+	unit->def += (newClass->baseDef - oldClass->baseDef);
+	unit->res += (newClass->baseRes - oldClass->baseRes);
+
+	unit->pClassData = newClass;
+
+	HideUnitSMS(unit);
+	MU_EndAll();
+	MU_Create(unit);
+}
---
+++ EngineHacks/TeleshackII/ThraciaASM/Dismount/Dismount.h
@@ -0,0 +1,21 @@
+
+void DismountRoutine(Proc* procState);
+void MountRoutine(Proc* procState);
+int DismountUsability();
+int MountUsability();
+const ClassData* GetDismountedClass(Unit* unit);
+const ClassData* GetMountedClass(Unit* unit);
+void UnitChangeClass(Unit* unit, const ClassData* newClass);
+
+typedef struct DismountEntry DismountEntry;
+
+struct DismountEntry{
+	/* 00 */ u8 mountedNumber;
+	/* 01 */ u8 dismountedNumber;
+};
+
+extern struct DismountEntry MountedClassTable[];
+
+#define USABILITY_TRUE 1
+#define USABILITY_GRAY 2
+#define USABILITY_FALSE 3
