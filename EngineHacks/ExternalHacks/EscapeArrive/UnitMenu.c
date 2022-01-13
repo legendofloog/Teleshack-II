6ac79932112dd92d9407b540cb8066681d2f97b4
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-01-12 21:34:33 -0800

    lotsa changes

--- EngineHacks/ExternalHacks/EscapeArrive/UnitMenu.c
+++ EngineHacks/ExternalHacks/EscapeArrive/UnitMenu.c
@@ -1,7 +1,7 @@
 #include "gbafe.h"
 
 int GetLocationEventCommandAt(int xPos, int yPos);
-
+void SeizeEffect();
 bool ArriveCommandUsability(){
     Unit* activeUnit = gActiveUnit;
     if(activeUnit->state & US_CANTOING){
@@ -24,4 +24,20 @@ bool EscapeCommandUsability(){
         return false;
     }
     return true;
+}
+
+void ArriveCommandEffect(){ //cheating (idc about arrive)
+    asm(push {r4,r14});
+    asm(ldr r4,=#0x8023021);
+    asm(mov r14,r4);
+    asm(.short 0xF800);
+    asm(mov r0,#0x94);
+    asm(pop {r4});
+    asm(pop {r1});
+    asm(bx r1);
+    }
+}
+
+void EscapeCommandEffect(){
+    
 }
