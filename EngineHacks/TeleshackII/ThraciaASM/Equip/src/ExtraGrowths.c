ff70c2c0652d53d969ddd0c60d5a7c32a3ac566c
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-06-26 22:41:45 -0700

    spell animations + class cards, misc

--- EngineHacks/TeleshackII/ThraciaASM/Equip/src/ExtraGrowths.c
+++ EngineHacks/TeleshackII/ThraciaASM/Equip/src/ExtraGrowths.c
@@ -30,100 +30,45 @@ void New_WriteGrowthsToBattleStruct(BattleUnit* battleUnit){
 		else {
 			battleUnit->unit.exp -= 100;
 		}
-
-		if (battleUnit->unit.maxHP < battleUnit->unit.pClassData->maxHP){
+		Unit* levelingUnit = GetUnitByCharId(battleUnit->unit.pCharacterData->number);
+		
+		if (levelingUnit->maxHP < battleUnit->unit.pClassData->maxHP){
 			battleUnit->changeHP = GetStatIncrease(gGetHPGrowth(&battleUnit->unit));
 		}
 
-		if (battleUnit->unit.pow < battleUnit->unit.pClassData->maxPow && GetMountedClass(&battleUnit->unit) == 0){
+		if (levelingUnit->pow < battleUnit->unit.pClassData->maxPow){
 			battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
 		}
-		else {
-			if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.pow + 1) < battleUnit->unit.pClassData->maxPow){ 
-				battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.pow + 2) < battleUnit->unit.pClassData->maxPow){ 
-				battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.pow + 1) < battleUnit->unit.pClassData->maxPow){ 
-				battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-			}
-			else{
 
-			}
-		}
-
-		if (battleUnit->unit.mag < MagClassTable[battleUnit->unit.pClassData->number].maxMag){
+		if (levelingUnit->mag < MagClassTable[battleUnit->unit.pClassData->number].maxMag){
 			battleUnit->changeMag = GetStatIncrease(gGetMagGrowth(&battleUnit->unit));
 		}
 
-		if (battleUnit->unit.skl < battleUnit->unit.pClassData->maxSkl && GetMountedClass(&battleUnit->unit) == 0){
+		if (levelingUnit->skl < battleUnit->unit.pClassData->maxSkl){
 			battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
 		}
-		else {
-			if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){ 
-				battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){ 
-				battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){
-				battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-			}
-			else{
 
-			}
-		}
-
-		if (battleUnit->unit.spd < battleUnit->unit.pClassData->maxSpd && GetMountedClass(&battleUnit->unit) == 0){
+		if (levelingUnit->spd < battleUnit->unit.pClassData->maxSpd){
 			battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
 		}
-		else {
-			if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.spd + 2) < battleUnit->unit.pClassData->maxSpd){ 
-				battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.spd + 1) < battleUnit->unit.pClassData->maxSpd){ 
-				battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.spd + 1) < battleUnit->unit.pClassData->maxSpd){ 
-				battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-			}
-			else{
-
-			}
-		}
-
-		if (battleUnit->unit.def < battleUnit->unit.pClassData->maxDef && GetMountedClass(&battleUnit->unit) == 0){
+		
+		if (levelingUnit->def < battleUnit->unit.pClassData->maxDef){
 			battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
 		}
-		else {
-			if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.def + 1) < battleUnit->unit.pClassData->maxDef){ 
-				battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.def + 1) < battleUnit->unit.pClassData->maxDef){ 
-				battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-			}
-			else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.def + 2) < battleUnit->unit.pClassData->maxDef){ 
-				battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-			}
-			else{
 
-			}
-		}
-
-		if (battleUnit->unit.res < battleUnit->unit.pClassData->maxRes){
+		if (levelingUnit->res < battleUnit->unit.pClassData->maxRes){
 			battleUnit->changeRes = GetStatIncrease(gGetResGrowth(&battleUnit->unit));
 		}
 
-		if (battleUnit->unit.lck < 20){
+		if (levelingUnit->lck < 20){
 			battleUnit->changeLck = GetStatIncrease(gGetLukGrowth(&battleUnit->unit));
 		}
 
-		if (battleUnit->unit.movBonus + battleUnit->unit.pClassData->baseMov < 15){
+		if (levelingUnit->movBonus + battleUnit->unit.pClassData->baseMov < 15){
 			battleUnit->changeMov = GetStatIncrease(GetMovGrowth(&battleUnit->unit));
 		}
 
-		if (battleUnit->unit.conBonus + battleUnit->unit.pClassData->baseCon < battleUnit->unit.pClassData->maxCon){
+		if (levelingUnit->conBonus + battleUnit->unit.pClassData->baseCon < battleUnit->unit.pClassData->maxCon){
 			battleUnit->changeCon = GetStatIncrease(GetConGrowth(&battleUnit->unit));
 		}
 
@@ -132,161 +77,70 @@ void New_WriteGrowthsToBattleStruct(BattleUnit* battleUnit){
 			int reroll = REROLL_ATTEMPTS;
 
 			for (int i = 0; i < reroll; i++){
-				if (battleUnit->unit.maxHP < battleUnit->unit.pClassData->maxHP){
+				if (levelingUnit->maxHP < battleUnit->unit.pClassData->maxHP){
 					battleUnit->changeHP = GetStatIncrease(gGetHPGrowth(&battleUnit->unit));
 					if (battleUnit->changeHP){
 						break;
 					}
 				}
 
-				if (battleUnit->unit.pow < battleUnit->unit.pClassData->maxPow && GetMountedClass(&battleUnit->unit) == 0){
+				if (levelingUnit->pow < battleUnit->unit.pClassData->maxPow){
 					battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
 					if (battleUnit->changePow){
 						break;
 					}
 				}
-				else {
-					if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.pow + 1) < battleUnit->unit.pClassData->maxPow){ 
-						battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-						if (battleUnit->changePow){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.pow + 2) < battleUnit->unit.pClassData->maxPow){ 
-						battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-						if (battleUnit->changePow){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.pow + 1) < battleUnit->unit.pClassData->maxPow){ 
-						battleUnit->changePow = GetStatIncrease(gGetStrGrowth(&battleUnit->unit));
-						if (battleUnit->changePow){
-							break;
-						}
-					}
-					else{
 
-					}
-				}
-
-				if (battleUnit->unit.mag < MagClassTable[battleUnit->unit.pClassData->number].maxMag){
+				if (levelingUnit->mag < MagClassTable[battleUnit->unit.pClassData->number].maxMag){
 					battleUnit->changeMag = GetStatIncrease(gGetMagGrowth(&battleUnit->unit));
 					if (battleUnit->changeMag){
 						break;
 					}
 				}
 
-				if (battleUnit->unit.skl < battleUnit->unit.pClassData->maxSkl && GetMountedClass(&battleUnit->unit) == 0){
+				if (levelingUnit->skl < battleUnit->unit.pClassData->maxSkl){
 					battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
 					if (battleUnit->changeSkl){
 						break;
 					}
 				}
-				else {
-					if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){ 
-						battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-						if (battleUnit->changeSkl){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){ 
-						battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-						if (battleUnit->changeSkl){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.skl + 1) < battleUnit->unit.pClassData->maxSkl){
-						battleUnit->changeSkl = GetStatIncrease(gGetSklGrowth(&battleUnit->unit));
-						if (battleUnit->changeSkl){
-							break;
-						}
-					}
-					else{
-
-					}
-				}
 				
-				if (battleUnit->unit.spd < battleUnit->unit.pClassData->maxSpd && GetMountedClass(&battleUnit->unit) == 0){
+				if (levelingUnit->spd < battleUnit->unit.pClassData->maxSpd){
 					battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
 					if (battleUnit->changeSpd){
 						break;
 					}
 				}
-				else {
-					if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.spd + 2) < battleUnit->unit.pClassData->maxSpd){ 
-						battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-						if (battleUnit->changeSpd){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.spd + 1) < battleUnit->unit.pClassData->maxSpd){ 
-						battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-						if (battleUnit->changeSpd){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.spd + 1) < battleUnit->unit.pClassData->maxSpd){ 
-						battleUnit->changeSpd = GetStatIncrease(gGetSpdGrowth(&battleUnit->unit));
-						if (battleUnit->changeSpd){
-							break;
-						}
-					}
-					else{
-
-					}
-				}
 				
-				if (battleUnit->unit.def < battleUnit->unit.pClassData->maxDef && GetMountedClass(&battleUnit->unit) == 0){
+				if (levelingUnit->def < battleUnit->unit.pClassData->maxDef){
 					battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
 					if (battleUnit->changeDef){
 						break;
 					}
 				}
-				else {
-					if (GetMountedClass(&battleUnit->unit)->attributes & CA_MOUNTED && (battleUnit->unit.def + 1) < battleUnit->unit.pClassData->maxDef){ 
-						battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-						if (battleUnit->changeDef){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_WYVERN && (battleUnit->unit.def + 1) < battleUnit->unit.pClassData->maxDef){ 
-						battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-						if (battleUnit->changeDef){
-							break;
-						}
-					}
-					else if (GetMountedClass(&battleUnit->unit)->attributes & CA_PEGASUS && (battleUnit->unit.def + 2) < battleUnit->unit.pClassData->maxDef){ 
-						battleUnit->changeDef = GetStatIncrease(gGetDefGrowth(&battleUnit->unit));
-						if (battleUnit->changeDef){
-							break;
-						}
-					}
-					else{
 
-					}
-				}
-				if (battleUnit->unit.res < battleUnit->unit.pClassData->maxRes){
+				if (levelingUnit->res < battleUnit->unit.pClassData->maxRes){
 					battleUnit->changeRes = GetStatIncrease(gGetResGrowth(&battleUnit->unit));
 					if (battleUnit->changeRes){
 						break;
 					}
 				}
 				
-				if (battleUnit->unit.lck < 30){
+				if (levelingUnit->lck < 20){
 					battleUnit->changeLck = GetStatIncrease(gGetLukGrowth(&battleUnit->unit));
 					if (battleUnit->changeLck){
 						break;
 					}
 				}
 				
-				if (battleUnit->unit.movBonus + battleUnit->unit.pClassData->baseMov < 15){
+				if (levelingUnit->movBonus + battleUnit->unit.pClassData->baseMov < 15){
 					battleUnit->changeMov = GetStatIncrease(GetMovGrowth(&battleUnit->unit));
 					if (battleUnit->changeMov){
 						break;
 					}
 				}
 				
-				if (battleUnit->unit.conBonus + battleUnit->unit.pClassData->baseCon < battleUnit->unit.pClassData->maxCon){
+				if (levelingUnit->conBonus + battleUnit->unit.pClassData->baseCon < battleUnit->unit.pClassData->maxCon){
 					battleUnit->changeCon = GetStatIncrease(GetConGrowth(&battleUnit->unit));
 					if (battleUnit->changeCon){
 						break;
