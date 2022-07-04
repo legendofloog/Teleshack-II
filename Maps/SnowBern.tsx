b768128c536ed33c803a27077086c3cd2990c987
Xilirite <99162031+Xilirite@users.noreply.github.com>
2022-07-04 02:56:53 -0400

    Chapter 24 Xil

---
+++ Maps/SnowBern.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="SnowBern" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../../../../ROMs/Assets/Revamped Castle Tileset/6C00A36E.png" width="512" height="512"/>
+</tileset>
