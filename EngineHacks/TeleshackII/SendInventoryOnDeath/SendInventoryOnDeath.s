8b46e9062e56e98f197745f592cce1d24e97bfe1
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-07-16 20:18:39 -0700

    quick hotfix

--- EngineHacks/TeleshackII/SendInventoryOnDeath/SendInventoryOnDeath.s
+++ EngineHacks/TeleshackII/SendInventoryOnDeath/SendInventoryOnDeath.s
@@ -20,9 +20,16 @@ mov    r1, #0x5
 orr    r0, r1
 str    r0, [r2, #0xC]
 
+ldrb	r0, [r2, #0xB]
+mov	r1, #0xC0
+and	r0, r1
+cmp	r0, #0x00 @ are they an allied unit
+bne	End	@ if not, do nothing
+
 mov    r0, r2
 blh    SendInventoryToConvoy, r3
 
+End:
 pop    {r2}
 ldr    r3, =0x8018420|1
 bx     r3
