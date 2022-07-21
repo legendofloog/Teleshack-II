14bd73e5b68eade70f47ec35c770ce6cba93c9d9
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-07-20 17:18:16 -0700

    new title screen,

--- Graphics/TitleScreen/256TitleScreen/titlebackgroundimage.h
+++ Graphics/TitleScreen/256TitleScreen/titlebackgroundimage.h
@@ -8,7 +8,7 @@
 //	+ 600 tiles not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2022-07-08, 17:41:10
+//	Time-stamp: 2022-07-20, 15:56:02
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
