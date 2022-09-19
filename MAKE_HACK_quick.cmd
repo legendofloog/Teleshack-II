89e51fbe387ced77d8f1b237299f8367d00bdd60
Pikmin1211 <joefarr1211@yahoo.ca>
2022-09-18 21:46:40 -0400

    tall windows, sound room, steal, unit menu

--- MAKE_HACK_quick.cmd
+++ MAKE_HACK_quick.cmd
@@ -1,3 +1,4 @@
+@echo off
 
 @rem using two scripts that do mostly the same thing is annoying to maintain tbh
 @rem so we let MAKE HACK_full do everything and do it slightly differently based on its argument
