bac6379aded54e25bb5d1d43f6ecff8ebeee788e
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-10-02 11:04:05 -0400

    minor fixes + last support

--- MakeHack.sh
+++ MakeHack.sh
@@ -9,16 +9,19 @@ base_dir=$(dirname "$(readlink -f "$0")")
 
 source_rom="$base_dir/FE8_clean.gba"
 
-main_event="$base_dir/ROM Buildfile.event"
+main_event="$base_dir/ROMBuildfile.event"
 
 target_rom="$base_dir/SkillsTest.gba"
 target_ups="$base_dir/SkillsTest.ups" # unused, but kept for symmetry with MAKE HACK_full.cmd
+target_sym="$base_dir/SkillsTest.sym"
 
 # defining tools
 
 c2ea_py="$base_dir/Tools/C2EA/c2ea.py"
 textprocess_py="$base_dir/Tools/TextProcess/text-process-classic.py"
-parsefile="$base_dir/Event Assembler/Tools/ParseFile"
+parsefile="$base_dir/EventAssembler/Tools/ParseFile"
+tmx2ea="$base_dir/Tools/tmx2ea/tmx2ea.py"
+symcombo="$base_dir/Tools/sym/SymCombo.py"
 
 # finding correct python version
 
@@ -56,13 +59,21 @@ if [[ $1 != quick ]]; then
 
   cd "$base_dir/Text"
   echo | $python3 "$textprocess_py" \
-    "text_buildfile.txt" --parser-exe "$parsefile"
+    "text_buildfile.txt" --parser-exe "$parsefile" --installer "InstallTextData.event" --definitions "TextDefinitions.event"
+    
+  echo "Processing Maps"
+  cd "$base_dir/Maps/"
+  echo | $python3 $tmx2ea -s -O "MasterMapInstaller.event"
 fi
 
 echo "Assembling"
 
-cd "$base_dir/Event Assembler"
-mono ColorzCore.exe A FE8 "-output:$target_rom" "-input:$main_event"
+cd "$base_dir/EventAssembler"
+./ColorzCore A FE8 "-output:$target_rom" "-input:$main_event" --nocash-sym
+
+echo Generating sym file
+
+$python3 $symcombo "$target_sym" "$target_sym" "$base_dir/Tools/sym/VanillaOffsets.sym"
 
 # TODO: generate patch (would require a linux version of ups)
 
