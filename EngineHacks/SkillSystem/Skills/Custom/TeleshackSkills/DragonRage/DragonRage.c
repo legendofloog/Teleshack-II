527226c108ba8aac52f5ef3473180cdabdfe1d23
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-10-08 15:01:02 -0400

    some more fixes

--- EngineHacks/SkillSystem/Skills/Custom/TeleshackSkills/DragonRage/DragonRage.c
+++ EngineHacks/SkillSystem/Skills/Custom/TeleshackSkills/DragonRage/DragonRage.c
@@ -1,8 +1,9 @@
 #include "DragonRage.h"
 
 void DragonRage(BattleUnit* unit1, BattleUnit* unit2){ //if initiating while fatigued with skill, attacks become brave
-	if((gSkillTester(&gBattleActor.unit, DragonRageIDLink)) && gBattleActor.unit.fatigue > gBattleActor.unit.maxHP){
-		BattleUnit* actorUnit = &gBattleActor;
-		actorUnit->weaponAttributes |= IA_BRAVE;
+	if((gSkillTester(&unit1->unit, DragonRageIDLink)) && unit1->unit.fatigue > unit1->unit.maxHP){
+		if (unit1->unit.pCharacterData->number == gBattleActor.unit.pCharacterData->number){
+			unit1->weaponAttributes |= IA_BRAVE;
+		}
 	}
 }
