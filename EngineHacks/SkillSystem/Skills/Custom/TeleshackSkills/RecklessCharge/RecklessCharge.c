c7008016397a2893268320f08b9e04369588065e
legendofloog <77558732+legendofloog@users.noreply.github.com>
2022-10-15 13:06:33 -0400

    reckless charge desc, 18 fix. 6 fix

--- EngineHacks/SkillSystem/Skills/Custom/TeleshackSkills/RecklessCharge/RecklessCharge.c
+++ EngineHacks/SkillSystem/Skills/Custom/TeleshackSkills/RecklessCharge/RecklessCharge.c
@@ -9,11 +9,7 @@ void RecklessCharge(BattleUnit* unit1, BattleUnit* unit2){
 	int tilesLeftToMove = actingUnit->movBonus - currentActionData.moveCount; //actiondata seems to work
 	// try to apply reckless charge
 	if((gSkillTester(actingUnit, RecklessChargeIDLink)) && (actingUnit->pClassData->attributes & CA_MOUNTED) && (tilesLeftToMove == 0)){
-		BattleUnit* actingBattleUnit = &gBattleActor;
-		BattleUnit* targetBattleUnit = &gBattleTarget;
-		actingBattleUnit->weaponAttributes |= IA_BRAVE; //acting battle unit weapon brave
-		//actingBattleUnit->battleHitRate = 0xFF; //target unit can't dodge
-		targetBattleUnit->battleHitRate = 0xFF; //acting unit can't dodge
+		unit1->weaponAttributes |= IA_BRAVE; //acting battle unit weapon brave
 	}
 }
 
